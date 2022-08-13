6eec6eb44f64a3867cbb8b8ffde9a1fa00adfcb3
m.gorshkov <m.gorshkov@skbkontur.ru>
2022-08-13 17:34:43 +0800

    exs10

---
+++ exp10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+using System;
+
+namespace exp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number= new Random().Next(100,1000);
+            int number1=number%100/10;
+            Console.WriteLine(number);
+            Console.Write(number1);
+        }
+    }
+}
