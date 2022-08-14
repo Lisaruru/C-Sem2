7285023bbe237ee9e81709f05e20c3758b7fa7bc
m.gorshkov <m.gorshkov@skbkontur.ru>
2022-08-14 15:16:12 +0800

    exs15

---
+++ exp15/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.//
+
+using System;
+
+namespace exp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число: ");
+            int day=Convert.ToInt32(Console.ReadLine());
+            if (day<=0 || day>7)
+            {
+                Console.WriteLine("Неправильное число");
+            }    
+
+            else
+            {
+                if (day>0 && day<6)
+                {
+                    Console.WriteLine("Рабочий день");
+                }
+
+                else
+                {
+                    Console.WriteLine("Выходной день");
+
+                }
+            }
+
+        }
+    }
+}
