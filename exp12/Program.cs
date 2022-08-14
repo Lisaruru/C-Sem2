c3d217753860c84cf78d8276f5a0b833a0c1bf3b
m.gorshkov <m.gorshkov@skbkontur.ru>
2022-08-14 14:47:11 +0800

    exs12

---
+++ exp12/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.//
+using System;
+
+namespace exp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            long usernumber;
+            long.TryParse(Console.ReadLine(), out usernumber);
+            if (usernumber <100)
+            {
+                Console.WriteLine("Третьей цифры нет");
+            }
+             else
+             {
+                while (usernumber>=1000)
+                {
+                    usernumber=usernumber/10;
+                 
+                }
+                
+                Console.WriteLine(usernumber%10);
+                
+
+               }
+
+        }
+   }
+}
